2cf7127a2c170246bfd59bb2c370d6618a621d7e
Elena Pavlova <epa@hellofresh.com>
2021-12-02 11:17:10 -0500

    Test

--- demo/scripts/linting.sh
+++ demo/scripts/linting.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "I'm doing linting! Add your commands here"
 
-#if [[ -z "${TEST_VAR}" ]]; then
-#  echo "TEST_VAR env var wasn't set"
-#  exit 1
-#else
-#  echo "TEST_VAR is saying: ${TEST_VAR}"
-#fi
+if [[ -z "${TEST_VAR}" ]]; then
+  echo "TEST_VAR env var wasn't set"
+  exit 1
+else
+  echo "TEST_VAR is saying: ${TEST_VAR}"
+fi
 
